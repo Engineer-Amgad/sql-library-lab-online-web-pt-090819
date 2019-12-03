9a8c70895fa9ba0d1e70d6c18b738a7a46b2a426
Amgad Malik <amgadmalik@yahoo.com>
2019-12-03 03:42:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,8 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
   INNER JOIN characters
-  ON series.author_id = characters.author_id;"
+  ON series.author_id = characters.author_id
+  WHERE characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
